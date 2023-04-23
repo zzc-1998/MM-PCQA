fcbb7014887cb501db586630828af3efbd98d7d9
zzc-1998 <58689334+zzc-1998@users.noreply.github.com>
2023-04-23 18:28:54 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@ CUDA_VISIBLE_DEVICES=0 python -u train.py \
 --model MM_PCQA \
 --batch_size  8 \
 --database SJTU  \
---data_dir_2d /DATA/zzc/3dqa/sjtu/sjtu_6face/ \
---data_dir_pc /DATA/zzc/3dqa/sjtu/sjtu_patch_2048/ \
+--data_dir_2d path_to_sjtu_projections/ \
+--data_dir_pc path_to_sjtu_patch_2048/ \
 --loss l2rank \
 --num_epochs 50 \
 --k_fold_num 9 \
@@ -16,8 +16,8 @@ CUDA_VISIBLE_DEVICES=0 python -u train.py \
 --model MM_PCQA \
 --batch_size  8 \
 --database WPC  \
---data_dir_2d /DATA/zzc/3dqa/wpc/wpc_6face/ \
---data_dir_pc /DATA/zzc/3dqa/wpc/wpc_patch_2048/ \
+--data_dir_2d path_to_wpc_projections/ \
+--data_dir_pc path_to_wpc_patch_2048/ \
 --loss l2rank \
 --num_epochs 50 \
 --k_fold_num 5 \
@@ -28,8 +28,8 @@ CUDA_VISIBLE_DEVICES=0 python -u train.py \
 --model MM_PCQA \
 --batch_size  8 \
 --database WPC2.0  \
---data_dir_2d /DATA/zzc/3dqa/wpc2.0/wpc2.0_6face/ \
---data_dir_pc /DATA/zzc/3dqa/wpc2.0/wpc2.0_patch_2048/ \
+--data_dir_2d path_to_wpc2.0_projections/ \
+--data_dir_pc path_to_wpc2.0_patch_2048/ \
 --loss l2rank \
 --num_epochs 50 \
 --k_fold_num 4 \
